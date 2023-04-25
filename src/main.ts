f078ab782d560574a7a212570450973f9ba48afe
chenbo <chenbo@huaweisoft.com>
2023-04-25 18:29:19 +0800

    菜单处理

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import { createPinia } from 'pinia'
-
-import App from './App.vue'
 import router from './router'
+import App from './App.vue'
 
-import './assets/main.css'
+import 'element-plus/dist/index.css'
+import '@/style/index.scss'
 
 const app = createApp(App)
-
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,22 +5,20 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/dynamic',
-      name: 'DynamicLoad',
-      component: () => import('../views/DynamicLoad.vue')
+      path: '/file',
+      name: '文件处理',
+      children: [
+        {
+          path: '/dynamic',
+          name: 'DynamicLoad',
+          component: () => import('../views/DynamicLoad.vue')
+        },
+        {
+          path: '/about',
+          name: 'about',
+          component: () => import('../views/AboutView.vue')
+        }
+      ]
     }
   ]
 })
