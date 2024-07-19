9a83a2f67cc88940d15c1832fe52fe242b8a42e8
chenbo <chenbo@huaweisoft.com>
2024-07-19 18:59:45 +0800

    增加其它的依赖

--- uno.config.js
+++ uno.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, presetUno } from 'unocss'
+import { defineConfig, presetUno, presetAttributify, presetIcons } from 'unocss'
 
 export default defineConfig({
   // rules: [
@@ -6,11 +6,14 @@ export default defineConfig({
   // ],
   presets: [
     presetUno(),
+    presetAttributify(), 
+    presetIcons()
   ],
   shortcuts: [
     {
       "h-btn": 'py-2 px-4 font-semibold rounded-lg shadow-md',
+      "h-btn-blue": 'bg-blue w-80 h-10 ml-10 m-1'
     },
-    [/^btn-(.*)$/, ([, c]) => `bg-${c}-400 text-${c}-100 py-2 px-4 rounded-lg`]
+    [/^btn-(.*)$/, ([, c]) => `bg-${c}-400 text-${c}-100 py-2 px-4 rounded-lg`],
   ],
 })
